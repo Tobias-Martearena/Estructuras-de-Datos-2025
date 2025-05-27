de18f29bd11a30b89490cbc52ba5a3d2f472fa9c
rTobi <tobiasmartearena2@gmail.com>
2025-05-27 13:52:24 -0300

    Se agrego un modulo listar por niveles

--- src/java/jerarquicas/ArbolBinario.java
+++ src/java/jerarquicas/ArbolBinario.java
@@ -1,78 +1,80 @@
 package java.jerarquicas;
+
 import java.lineales.dinamicas.Cola;
 import java.lineales.dinamicas.Lista;
 
 public class ArbolBinario {
 
-   private NodoArbol raiz;
+    private NodoArbol raiz;
 
-   public ArbolBinario(){
-    this.raiz = null;
-   }
+    public ArbolBinario() {
+        this.raiz = null;
+    }
 //-----------------------------------------------------------------------------------------//
 //Posible arreglo...
-    public boolean insertar(Object nuevoElem, Object elemPadre, char posHijo){
+
+    public boolean insertar(Object nuevoElem, Object elemPadre, char posHijo) {
         boolean exito = true;
-        if(this.raiz == null){
+        if (this.raiz == null) {
             this.raiz = new NodoArbol(nuevoElem);
-        }else{
+        } else {
             NodoArbol nPadre = obtenerNodo(this.raiz, elemPadre);
             //si padre existe y lugar no esta ocupado lo pone, sino da error
-            if(nPadre != null){
-                if(posHijo == 'I' && nPadre.getIzquierdo() == null){
+            if (nPadre != null) {
+                if (posHijo == 'I' && nPadre.getIzquierdo() == null) {
                     nPadre.setIzquierdo(new NodoArbol(nuevoElem));
-                }else{
-                    if(posHijo == 'D' && nPadre.getDerecho() == null){
+                } else {
+                    if (posHijo == 'D' && nPadre.getDerecho() == null) {
                         nPadre.setDerecho(new NodoArbol(nuevoElem));
-                    }else{
+                    } else {
                         exito = false;
                     }
                 }
 
-            }else{
+            } else {
                 exito = false;
             }
         }
         return exito;
     }
 
-    public boolean insertarPorPosicion(Object nuevoElem, int pos, char posHijo){
+    public boolean insertarPorPosicion(Object nuevoElem, int pos, char posHijo) {
         boolean exito = true;
-        if(this.raiz == null){
+        if (this.raiz == null) {
             this.raiz = new NodoArbol(nuevoElem);
-        }else{
+        } else {
             Lista lista = listarPorNivel();
-            if(pos > 0 && pos<=lista.longitud()){
-                NodoArbol padre = (NodoArbol)lista.recuperar(pos);
-                if(padre.getIzquierdo()==null){
+            if (pos > 0 && pos <= lista.longitud()) {
+                NodoArbol padre = (NodoArbol) lista.recuperar(pos);
+                if (padre.getIzquierdo() == null) {
                     padre.setIzquierdo(new NodoArbol(nuevoElem));
-                }else{
-                    if(padre.getDerecho()==null){
+                } else {
+                    if (padre.getDerecho() == null) {
                         padre.setDerecho(new NodoArbol(nuevoElem));
-                    }else{
+                    } else {
                         exito = false;
                     }
                 }
-            }else{
+            } else {
                 exito = false;
             }
         }
         return exito;
     }
 
-    private NodoArbol obtenerNodo(NodoArbol n, Object buscado){
+    private NodoArbol obtenerNodo(NodoArbol n, Object buscado) {
         //metodo PRIVADO que busca un elemento y devuelve el nodo
         // que lo contiene. Si no se encuentra buscado devuelve null
         NodoArbol resultado = null;
-        if(n != null){
-            if(n.getElem().equals(buscado)){
+        if (n != null) {
+            if (n.getElem().equals(buscado)) {
                 //si el buscado es n, lo devuelve
                 resultado = n;
-            }else{
+            } else {
                 //no es el buscado: busca primero en el HI
                 resultado = obtenerNodo(n.getIzquierdo(), buscado);
                 //si no lo encontro en el HI, busca en HD
-                if(resultado == null){
+                if (resultado == null) {
                     resultado = obtenerNodo(n.getDerecho(), buscado);
                 }
             }
@@ -80,19 +82,19 @@ public class ArbolBinario {
         return resultado;
     }
 
-    public Lista listarPorNivel(){
+    public Lista listarPorNivel() {
         Lista lista = new Lista();
         Cola cola = new Cola();
-        if(this.raiz!= null){
+        if (this.raiz != null) {
             cola.poner(this.raiz);
             while (!cola.esVacia()) {
-                NodoArbol nodo =(NodoArbol)cola.obtenerFrente();
+                NodoArbol nodo = (NodoArbol) cola.obtenerFrente();
                 cola.sacar();
-                lista.insertar(nodo.getElem(), lista.longitud()+1);
-                if(nodo.getIzquierdo()!= null){
+                lista.insertar(nodo.getElem(), lista.longitud() + 1);
+                if (nodo.getIzquierdo() != null) {
                     cola.poner(nodo.getIzquierdo());
-                }else{
-                    if(nodo.getDerecho()!=null){
+                } else {
+                    if (nodo.getDerecho() != null) {
                         cola.poner(nodo.getDerecho());
                     }
                 }
@@ -102,50 +104,50 @@ public class ArbolBinario {
     }
 //-----------------------------------------------------------------------------------------//
 
-    public boolean esVacio(){
+    public boolean esVacio() {
         boolean exito = true;
-        if(this.raiz != null){
+        if (this.raiz != null) {
             exito = false;
         }
         return exito;
     }
 
-    public int altura(){
+    public int altura() {
         return alturaAux(this.raiz);
     }
 
-    private int alturaAux(NodoArbol nodo){
+    private int alturaAux(NodoArbol nodo) {
         int res = 0;
-        if(nodo!=null){
+        if (nodo != null) {
             int izq = alturaAux(nodo.getIzquierdo());
             int der = alturaAux(nodo.getDerecho());
-            if(izq > der){
-                res = izq+1;
-            }else{
-                res = der+1;
+            if (izq > der) {
+                res = izq + 1;
+            } else {
+                res = der + 1;
             }
-        }else{
+        } else {
             res = -1;
         }
         return res;
     }
 
-    public int nivel(Object elemento){
+    public int nivel(Object elemento) {
         return nivelAux(this.raiz, elemento, -1);
     }
 
-    private int nivelAux(NodoArbol nodo,Object elem, int altura){
+    private int nivelAux(NodoArbol nodo, Object elem, int altura) {
         int alturaAux = -1;
         altura++;
-        if(nodo != null){
-            if(nodo.getElem().equals(elem)){
+        if (nodo != null) {
+            if (nodo.getElem().equals(elem)) {
                 alturaAux = altura;
-            }else{
-                if(nodo.getIzquierdo() != null){
+            } else {
+                if (nodo.getIzquierdo() != null) {
                     alturaAux = nivelAux(nodo.getIzquierdo(), elem, altura);
-                }else{
-                    if(alturaAux == -1){
-                        if(nodo.getDerecho() != null){
+                } else {
+                    if (alturaAux == -1) {
+                        if (nodo.getDerecho() != null) {
                             alturaAux = nivelAux(nodo.getDerecho(), elem, altura);
                         }
                     }
@@ -161,16 +163,16 @@ public class ArbolBinario {
 
     private Object padreRec(NodoArbol nodo, Object elem) {
         Object res = null;
-        if(nodo != null){
-            if(nodo.getIzquierdo() != null && nodo.getIzquierdo().getElem().equals(elem)){
+        if (nodo != null) {
+            if (nodo.getIzquierdo() != null && nodo.getIzquierdo().getElem().equals(elem)) {
                 res = nodo.getElem();
-            }else{
-                if(nodo.getDerecho() != null && nodo.getDerecho().getElem().equals(elem)){
+            } else {
+                if (nodo.getDerecho() != null && nodo.getDerecho().getElem().equals(elem)) {
                     res = nodo.getElem();
-                }else{
+                } else {
                     res = padreRec(nodo.getIzquierdo(), elem);
 
-                    if(res == null){
+                    if (res == null) {
                         res = padreRec(nodo.getDerecho(), elem);
                     }
                 }
@@ -178,17 +180,17 @@ public class ArbolBinario {
         }
         return res;
     }
-    
-    public Lista listarPreorden(){
+
+    public Lista listarPreorden() {
         Lista lista = new Lista();
         listarPreordenAux(this.raiz, lista);
         return lista;
     }
 
-    private void listarPreordenAux(NodoArbol nodo, Lista lis){
-        if(nodo != null){
+    private void listarPreordenAux(NodoArbol nodo, Lista lis) {
+        if (nodo != null) {
             //Inserta en la lista el elemento
-            lis.insertar(nodo.getElem(), lis.longitud()+1);
+            lis.insertar(nodo.getElem(), lis.longitud() + 1);
 
             //Recorre sus hijos en preOrden
             listarPreordenAux(nodo.getIzquierdo(), lis);
@@ -196,31 +198,51 @@ public class ArbolBinario {
         }
     }
 
-    public Lista listarInorden(){
+    public Lista listarInorden() {
         Lista lista = new Lista();
         listarInordenAux(this.raiz, lista);
         return lista;
     }
 
-    private void listarInordenAux(NodoArbol nodo,Lista lis){
-        if(nodo != null){
+    private void listarInordenAux(NodoArbol nodo, Lista lis) {
+        if (nodo != null) {
             listarInordenAux(nodo.getIzquierdo(), lis);
-            lis.insertar(nodo.getElem(), lis.longitud()+1);
+            lis.insertar(nodo.getElem(), lis.longitud() + 1);
             listarInordenAux(nodo.getDerecho(), lis);
         }
     }
 
-    public Lista listarPosorden(){
+    public Lista listarPosorden() {
         Lista lista = new Lista();
         listarPosordenAux(this.raiz, lista);
         return lista;
     }
 
-    private void listarPosordenAux(NodoArbol nodo, Lista lis){
-        if(nodo != null){
+    private void listarPosordenAux(NodoArbol nodo, Lista lis) {
+        if (nodo != null) {
             listarPosordenAux(nodo.getIzquierdo(), lis);
             listarPosordenAux(nodo.getDerecho(), lis);
-            lis.insertar(nodo.getElem(), lis.longitud()+1);
+            lis.insertar(nodo.getElem(), lis.longitud() + 1);
+        }
+    }
+
+    public Lista listarPorNiveles() {
+        Lista lista = new Lista();
+        int altura = altura();
+        for (int i = 0; i <= altura; i++) {
+            listarPorNivelesAux(this.raiz, lista, 0);
+        }
+        return lista;
+    }
+
+    private void listarPorNivelesAux(NodoArbol nodo, Lista lis, int nivel) {
+        if (nodo != null) {
+            if (nivel == 0) {
+                lis.insertar(nodo.getElem(), lis.longitud() + 1);
+            } else {
+                listarPorNivelesAux(nodo.getIzquierdo(), lis, nivel - 1);
+                listarPorNivelesAux(nodo.getDerecho(), lis, nivel - 1);
+            }
         }
     }
 }
