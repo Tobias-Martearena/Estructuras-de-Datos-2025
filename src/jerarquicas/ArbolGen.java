ee283ae0e3db8768ef32e54bdca9156ef7ff92d2
rTobi <tobiasmartearena2@gmail.com>
2025-06-01 15:07:02 -0300

    Se agrego insertarPorPosicion, insertarPorPosicionAux y obtenerNodoPorPos

--- src/jerarquicas/ArbolGen.java
+++ src/jerarquicas/ArbolGen.java
@@ -44,4 +44,53 @@ public class ArbolGen {
         }
         return nodoPadre;
     }
+
+    public boolean insertarPorPosicion(Object elem, int posicion){
+        boolean exito = false;
+        if(posicion > 0){
+            int[] arreglo = new int[1];
+            arreglo[0] = 1;
+            exito = insertarPorPosicionAux(this.raiz, elem, arreglo, posicion);
+        }
+        return exito;
+    }
+
+    public boolean insertarPorPosicionAux(NodoGen nodo, Object elemento, int[] matriz, int pos ){
+        boolean exito = true;
+        if(this.raiz == null){
+            this.raiz = new NodoGen(elemento);
+        }else{
+            NodoGen padre = obtenerNodoPorPos(this.raiz, pos, matriz);
+            if(padre != null){
+                NodoGen nuevoNodo = new NodoGen(elemento);
+                NodoGen hijo = padre.getHijoIzquierdo();
+                if(hijo == null){
+                    padre.setHijoIzquierdo(nuevoNodo);
+                }else{
+                    while(hijo.getHermanoDerecho() != null){
+                        hijo = hijo.getHermanoDerecho();
+                    }
+                    hijo.setHermanoDerecho(nuevoNodo);
+                }
+            }
+        }
+        return exito;
+    }
+
+    private NodoGen obtenerNodoPorPos(NodoGen nodo, int posicion, int[] arreglo){
+        NodoGen nodoRes = null;
+        if(nodo != null){
+            if(arreglo[0] == posicion){
+                nodoRes = nodo;
+            }else{
+                NodoGen hijo = nodo.getHijoIzquierdo();
+                while(hijo != null && nodoRes == null){
+                    arreglo[0] = arreglo[0] + 1;
+                    nodoRes = obtenerNodoPorPos(hijo, posicion, arreglo);
+                    hijo = hijo.getHermanoDerecho();
+                }
+            }
+        }
+        return nodoRes;
+    }
 }
