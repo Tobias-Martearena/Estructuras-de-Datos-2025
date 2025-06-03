2d7dee248c841a75cceb7e38b429662ab86c808a
rTobi <tobiasmartearena2@gmail.com>
2025-06-03 15:19:22 -0300

    Se agrego el metodo insertar e insertarAux en ArbolABB

--- src/main/java/conjuntistas/ArbolABB.java
+++ src/main/java/conjuntistas/ArbolABB.java
@@ -1,5 +1,40 @@
 package main.java.conjuntistas;
 
 public class ArbolABB {
-    
+
+    private NodoABB raiz;
+
+    public ArbolABB(){
+        this.raiz = null;
+    }
+
+    public boolean insertar(Comparable elemento){
+        boolean exito = true;
+        if(this.raiz == null){
+            this.raiz = new NodoABB(elemento);
+        }else{
+            exito = insertarAux(this.raiz, elemento);
+        }
+        return exito;
+    }
+
+    public boolean insertarAux(NodoABB nodo, Comparable elem) {
+        boolean exito = true;
+        if ((elem.compareTo(nodo.getElem())) == 0) {
+            exito = false;
+        } else if (elem.compareTo(nodo.getElem()) < 0) {
+            if (nodo.getIzquierdo() != null) {
+                exito = insertarAux(nodo.getIzquierdo(), elem);
+            } else {
+                nodo.setIzquierdo(new NodoABB(elem));
+            }
+        } else {
+            if (nodo.getDerecho() != null) {
+                exito = insertarAux(nodo.getDerecho(), elem);
+            } else {
+                nodo.setDerecho(new NodoABB(elem));
+            }
+        }
+        return exito;
+    }
 }
