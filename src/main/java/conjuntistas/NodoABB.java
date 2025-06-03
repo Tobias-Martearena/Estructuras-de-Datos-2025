148fc92ccfd55d79cc76d0dab83a8050eff68fdb
rTobi <tobiasmartearena2@gmail.com>
2025-06-03 14:52:27 -0300

    Se agrego el constructor en NodoABB

--- src/main/java/conjuntistas/NodoABB.java
+++ src/main/java/conjuntistas/NodoABB.java
@@ -1,7 +1,19 @@
 package main.java.conjuntistas;
 
 public class NodoABB {
-    Comparable elem;
-    NodoABB izquierdo;
-    NodoABB derecho;
+    private Comparable elem;
+    private NodoABB izquierdo;
+    private NodoABB derecho;
+
+    public NodoABB(Comparable elemento){
+        this.elem = elemento;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+
+    public NodoABB(Comparable elemento, NodoABB hijoIzq, NodoABB hijoDer){
+        this.elem = elemento;
+        this.izquierdo = hijoIzq;
+        this.derecho = hijoDer;
+    }
 }
